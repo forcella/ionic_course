5c61d4fe24c17da3ded4c636527322e5f0a22b48
Lucas Guerra <luguerra@live.com>
2019-11-18 07:19:19 -0300

    added graphql codegen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,39 +1,31 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonList,
-  IonItem,
-} from '@ionic/react'
-import React from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { LAUNCHES_PAST_QUERY } from '../graphql/launches'
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem } from '@ionic/react';
+import React from 'react';
+import { useLaunchesPastQuery } from '../generated/graphql';
 
 const Home: React.FC = () => {
-  const { data, loading, error } = useQuery(LAUNCHES_PAST_QUERY)
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic APP</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          data &&
-          data.launchesPast.map((o: any) => (
-            <IonItem key={o.id}>
-              {o.mission_name} | {o.rocket.rocket_name}
-            </IonItem>
-          ))
-        )}
-      </IonContent>
-    </IonPage>
-  )
-}
+	const { data, loading, error } = useLaunchesPastQuery();
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Ionic APP</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent className="ion-padding">
+				{error && error}
+				{loading ? (
+					<p>Loading...</p>
+				) : (
+					data &&
+					data.launchesPast!.map((o) => (
+						<IonItem key={o.id}>
+							{o.mission_name} | {o.rocket!.rocket_name}
+						</IonItem>
+					))
+				)}
+			</IonContent>
+		</IonPage>
+	);
+};
 
-export default Home
+export default Home;
